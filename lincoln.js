c02e94f83501fd735feae4aa1563ddb2c2453c06
davie.janeway <davie.janeway@btinternet.com>
2013-01-19 13:43:48 +0000

    WIP

--- lincoln.js
+++ lincoln.js
@@ -1,15 +1,35 @@
-var TEST = false;
+"use strict";
 
-var reserved = ['select', 'from', 'where', '=', '>', '<', '>=', '<=', '!=', 'order', 'group', 'by', 'limit']
+(function(exports) {
 
+/** SQL keywords */
+var reserved  = ['select', 'from', 'where', 'order', 'group', 'by', 'limit', 'and', 'or', 'like', 'null'];
+/** Relationship operators */
+var relations = ['=', '!=', '>', '<', '>=', '<=', 'is', 'not'];
+
+/** debugging flag */
+exports.__debug__ = false;
+
+/**
+ * Jump out at the first malformed token,
+ * printing a useful error message.
+ *
+ * @param msg - a concise, succinct error message
+ */
+function error(msg) {
+  if(!__debug__) {
+    print(msg);
+    throw null;
+  }
+}
 
 /**
  * Lex the input stream into a set of tokens
  * and report any malformed expressions.
  * 
- * @arg input
+ * @param input - the (sql statement)+ to parse
  */
-function tokenise(input) {
+exports.tokenise = function(input) {
   var tokens = [];
 
   var ch = '', buffer = '';
@@ -20,6 +40,8 @@ function tokenise(input) {
       // We gots us a token, store it & clear the buffer.
       if(reserved.indexOf(buffer) > -1) {
         tokens.push({type:'reserved', value:buffer});
+      } else if(relations.indexOf(buffer) > -1) {
+        tokens.push({type:'rel', value:buffer});
       } else if(buffer.length > 0) {
         tokens.push({type:'id', value:buffer});
       }
@@ -40,6 +62,12 @@ function tokenise(input) {
   return tokens.reverse();
 }
 
+/**
+ * Gets the list of columns to retrieve from
+ * the query  or all columns if token is '*'.
+ *
+ * @param tokens - list of token objects to parse
+ */
 function parseFields(tokens) {
   var ast = [];
   var field;
@@ -56,22 +84,87 @@ function parseFields(tokens) {
   return ast;
 }
 
+function parseLiteral(tokens) {
+  var literal = tokens.pop();
+  
+  if(literal.type !== 'id') {
+    error('Parse Error: expected column name or literal, found ' + literal.value);
+  }
+  
+  return literal;
+}
+
+function parseRelationship(tokens) {
+  var rel = tokens.pop();
+  
+  if(rel.type !== 'rel') {
+    error('Parse Error: expected relationship operator, found ' + rel.value);
+  }
+  
+  return rel;
+}
+
 function parseFrom(tokens) {
   var from = tokens.pop();
   
   if(from.value !== 'from') {
-    print('Parse Error: expected \'from\', found ' + from.value);
-    throw null;
+    error('Parse Error: expected \'from\', found ' + from.value);
   }
   
   from = tokens.pop();
   
   if(from.type !== 'id') {
-    print('Parse Error: expected table identifier, found ' + from.value);
-    throw null;
+    error('Parse Error: expected table identifier, found ' + from.value);
   }
   
   return from.value;
+}
+
+function parseExpression(tokens) {
+  var ast = {
+    lval: {},
+    rel: {},
+    rval: {}
+  };
+  
+  var lval = parseIdentifier();  
+  var rel  = parseRelationship();
+  var rval = parseIdentifier();
+  
+  ast.lval = lval;
+  ast.rel  = rel;
+  ast.rval = rval;
+  
+  return ast;
+}
+
+function parseClause(tokens) {
+  var ast = {
+    left: {},
+    right: {},
+    rel: {}
+  };
+  
+  ast.left  = parseExpression(tokens);
+  ast.rel   = parseRelationship(tokens);
+  ast.right = parseExpression(tokens);
+}
+
+function parseWhere(tokens) {
+  var ast = {
+    where: {
+      clauses:[]
+    }
+  };
+  
+  var clause;
+  while(clause = tokens.pop(), clause.value === 'and') {
+    if(clause.type !== 'id') {
+      error('Parse Error: expected expression, found ' + clause.value);
+    }
+    
+    ast.where.clauses.push(parseClause(tokens));
+  }
 }
 
 function parseSelect(tokens) {
@@ -84,7 +177,7 @@ function parseSelect(tokens) {
   
   ast.select.fields = parseFields(tokens);
   ast.select.from = parseFrom(tokens);
-
+  
   return ast;
 }
 
@@ -93,11 +186,13 @@ function parse(tokens) {
     return false;
   }
   
-  var head = tokens.pop();
-    
-  switch(head.value) {
-    case 'select': return parseSelect(tokens);
-    default: print('Parse Error: unexpected ' + head.value + ''); throw null;
+  var head;
+  while(head = tokens.pop(), tokens.length > 0) {
+    switch(head.value) {
+      case 'select': return parseSelect(tokens);
+      case 'where':  return parseWhere(tokens);
+      default: error('Parse Error: unexpected ' + head.value + '');
+    }
   }
 }
 
@@ -124,3 +219,11 @@ function translate(ast) {
     return generateFind(ast['select']);
   }
 }
+
+
+function sql_to_mongo(query) {
+  return translate(parse(tokenise(query)));
+}
+}(typeof exports === 'undefined'
+  ? (this.lincoln = {})
+  : exports));
--- tests.js
+++ tests.js
@@ -1,8 +1,8 @@
-exports. __test__ = function(assertion) {  
-  this.assert = function() {
-    if(eval(assertion) === false) {
-      console.log('Assertion failed - ' + assertion);
-      throw null;
-    }
-  }
-}
+var lincoln = require('./lincoln.js');
+
+lincoln.__debug__ = true;
+
+(function test_tokenise_select() {
+  var tokens = lincoln.tokenise('select;');
+  console.log(tokens);
+}());
